c33c628c71087c4aee9628bee47546e0a3ad4cb4
Boston Rohan <bostonrohan@gmail.com>
2021-08-04 09:01:41 -0700

    Solved and Refactored RGB to Hex Solution

--- 5Kyu/RGB to Hex Converison.js
+++ 5Kyu/RGB to Hex Converison.js
@@ -1,7 +1,10 @@
+//Main function that returns a string of all of the Hex Conversions.
 function rgb(r, g, b) {
   return [toHex(r), toHex(g), toHex(b)].join("");
 }
+//Helper function that converts a number to Hex.
 function toHex(num) {
+  //Object storing the decimal and Hex values.
   const conversion = {
     0: 0,
     1: 1,
@@ -20,27 +23,30 @@ function toHex(num) {
     14: "E",
     15: "F",
   };
+  //If the number is less than or equal to 0, return 00 as the Hex.
   if (num <= 0) {
-    ans = ["00"];
-    return ans;
-  } else {
-    num > 255 ? (num = 255) : (num = num);
-    var hex = num / 16;
-    var remainder = "0." + hex.toString().split(".")[1];
-    return [Math.floor(hex), parseFloat(remainder * 16)]
-      .map((num) => conversion[num])
-      .sort()
-      .join("");
+    return "00";
   }
+  //If the number is greater than 255 cap it off at 255. *Hex values cannot be above 255*.
+  num > 255 ? (num = 255) : (num = num);
+  //Establish variables
+  var hex = num / 16;
+  var remainder = "0." + hex.toString().split(".")[1];
+  //If there is no remainder, then assign it to zero.
+  hex % 1 == 0 ? (remainder = 0) : (remainder = remainder);
+  //Return the calculations of the Hex and remainder values, mapped with their respective conversions, joined into a string.
+  return [Math.floor(hex), parseFloat(remainder * 16)]
+    .map((num) => conversion[num])
+    .join("");
 }
-console.log(rgb(105, 114, 279));
+rgb(173, 255, 47);
+
 //Psuedo Code
 //Create an object that stores the Decimal, and Hexidecimal pairs, with the key value being the Decimal, and the Hexidecimal being the value.
 //Send each number through toHex function, which will return the two converted values.
 //To convert, divide the first number by 16, and round down.
 //To get second value, multiply the remainder by 16.
-
-//Sort each array in place and return them joined together.
+//Map the conversion and return as a string.
 
 //RGB to HEX reference: https://www.binaryhexconverter.com/decimal-to-hex-converter
 
