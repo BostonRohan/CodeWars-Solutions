e0b8651edad2bc75ddfc6f9bd37ec02514475096
Boston Rohan <bostonrohan@gmail.com>
2021-08-22 18:01:20 -0700

    solved Pete the baker

--- 5Kyu/Pete, the baker.js
+++ 5Kyu/Pete, the baker.js
@@ -1,26 +1,39 @@
 function cakes(recipe, available) {
-  if (checkIngredients(recipe, available) && checkAmount(recipe, available)) {
-    return checkAmount(recipe, available);
-  } else {
+  const recipeArr = Object.entries(recipe).sort(),
+    availableArr = Object.entries(available).sort();
+  var sortedAvailableAmount = [];
+  var sortedRecipeAmount = [];
+  if (recipeArr.length > availableArr.length) {
     return 0;
+  } else {
+    for (var i = 0; i < recipeArr.length; i++) {
+      for (var j = 0; j < availableArr.length; j++) {
+        if (availableArr[j][0] === recipeArr[i][0]) {
+          sortedAvailableAmount.push(availableArr[j][1]);
+          sortedRecipeAmount.push(recipeArr[i][1]);
+        }
+      }
+    }
   }
+  return Math.min(...checkAmount(sortedRecipeAmount, sortedAvailableAmount));
 }
-function checkAmount(recipe, available) {
-  var recipeAmount = Object.values(recipe),
-    availableAmount = Object.values(available);
-}
-function checkIngredients(recipe, available) {
-  var recipeArr = Object.keys(recipe).sort(),
-    availableArr = Object.keys(available).sort();
-  if (availableArr.includes(...recipeArr)) {
-    return true;
+function checkAmount(recipeArr, availableArr) {
+  for (var i = 0; i < recipeArr.length; i++) {
+    if (recipeArr[i] === availableArr[i]) {
+      return [1];
+    } else {
+      while (availableArr[i] >= 1) {
+        availableArr[i] = Math.floor(availableArr[i] / recipeArr[i]);
+        i++;
+      }
+    }
   }
-  return false;
+  return availableArr;
 }
 console.log(
   cakes(
-    { flour: 500, sugar: 200, eggs: 1 },
-    { flour: 1200, sugar: 1200, eggs: 5, milk: 200 }
+    { flour: 500, sugar: 500, eggs: 5 },
+    { flour: 500, sugar: 500, eggs: 5 }
   )
 );
 //Psuedo Code
