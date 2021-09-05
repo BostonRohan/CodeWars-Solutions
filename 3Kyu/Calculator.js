9fed12bd42937d3fc1678e7dd5ecb6bd3a0c9b27
Boston Rohan <bostonrohan@gmail.com>
2021-09-05 16:38:38 -0700

    started order of operations functionality

--- 3Kyu/Calculator.js
+++ 3Kyu/Calculator.js
@@ -1,14 +1,36 @@
 const Calculator = function () {
   this.evaluate = (string) => {
     if (/[+--]/g.test(string) && /[*\/]/g.test(string)) {
-      return "yes nigga";
+      string = string.split(" ");
+      var multPosition = string.indexOf("*"),
+        divPosition = string.indexOf("/");
+      //Check position before while loop.
+      //Possibly seperate while loops for mult and div?
+      //Possibly one while loop from the operate function?
+      while (divPosition && multPosition > 0) {
+        divPosition < multPosition
+          ? string.splice(
+              divPosition - 1,
+              3,
+              parseInt(string[divPosition - 1]) /
+                parseInt(string[divPosition + 1])
+            )
+          : string.splice(
+              multPosition - 1,
+              3,
+              divPosition - 1,
+              3,
+              parseInt(string[multPosition - 1]) *
+                parseInt(string[multPosition + 1])
+            );
+      }
+      return string;
     } else {
       return operate(string);
     }
     function operate(string) {
       string = string.split(" ");
       for (var i = 0; i < string.length; i++) {
-        console.log(string);
         if (string[i] === "*" && string[i + 1] && string[i - 1]) {
           string.splice(
             i - 1,
@@ -44,7 +66,7 @@ const Calculator = function () {
   };
 };
 var calculate = new Calculator();
-console.log(calculate.evaluate("30 + 23 + 77"));
+console.log(calculate.evaluate("2 / 2 + 3 * 4 - 6"));
 
 //Psuedo Code
 //{Refactor possibly with while loop?}
