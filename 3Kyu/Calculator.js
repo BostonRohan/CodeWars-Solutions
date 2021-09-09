229a5ce1f28eca36abafadccdce7d2cbc181472b
Boston Rohan <bostonrohan@gmail.com>
2021-09-08 20:09:58 -0700

    improved flawed logic with order of operations

--- 3Kyu/Calculator.js
+++ 3Kyu/Calculator.js
@@ -1,67 +1,47 @@
 const Calculator = function () {
   this.evaluate = (string) => {
     if (/[+--]/g.test(string) && /[*\/]/g.test(string)) {
-      string = string.split(" ");
-      var multPosition = string.indexOf("*"),
-        divPosition = string.indexOf("/");
-      //Check position before while loop.
-      //Possibly seperate while loops for mult and div?
-      //Possibly one while loop from the operate function?
-      while (divPosition && multPosition > 0) {
-        divPosition < multPosition
-          ? string.splice(
-              divPosition - 1,
-              3,
-              parseInt(string[divPosition - 1]) /
-                parseInt(string[divPosition + 1])
-            )
-          : string.splice(
-              multPosition - 1,
-              3,
-              divPosition - 1,
-              3,
-              parseInt(string[multPosition - 1]) *
-                parseInt(string[multPosition + 1])
-            );
+      var multIndex = [],
+        divIndex = [];
+      let arr = string.split(" ");
+      for (var i = 0; i < arr.length; i++) {
+        if (arr[i] === "/") {
+          divIndex.push(i);
+        } else if (arr[i] === "*") {
+          multIndex.push(i);
+        }
       }
-      return string;
+      return operate(string, multIndex, divIndex);
     } else {
       return operate(string);
     }
-    function operate(string) {
-      string = string.split(" ");
-      for (var i = 0; i < string.length; i++) {
-        if (string[i] === "*" && string[i + 1] && string[i - 1]) {
-          string.splice(
-            i - 1,
-            3,
-            parseInt(string[i - 1]) * parseInt(string[i + 1])
-          );
-          i = 0;
-        } else if (string[i] === "/" && string[i + 1] && string[i - 1]) {
-          string.splice(
-            i - 1,
-            3,
-            parseInt(string[i - 1]) / parseInt(string[i + 1])
-          );
-          i = 0;
-        } else if (string[i] === "+" && string[i + 1] && string[i - 1]) {
-          string.splice(
-            i - 1,
-            3,
-            parseInt(string[i - 1]) + parseInt(string[i + 1])
-          );
-          i = 0;
-        } else if (string[i] === "-" && string[i + 1] && string[i - 1]) {
-          string.splice(
-            i - 1,
-            3,
-            parseInt(string[i - 1]) - parseInt(string[i + 1])
-          );
-          i = 0;
+    function operate(string, multIndex, divIndex) {
+      //For loop to check both multIndex and divIndex, use while loop later...
+      let arr = string.split(" ");
+      let i = 0;
+      if (Math.min(...multIndex) > Math.min(...divIndex)) {
+        while (divIndex.length > 0) {
+          divIndex.splice(i, i);
+          arr.splice(arr[divIndex - 1] / arr[divIndex + 1]);
+          i++;
         }
       }
-      return string.join("");
+      //   for (var i = 0; i < arr.length; i++) {
+      //     if (arr[i] === "*" && arr[i + 1] && arr[i - 1]) {
+      //       arr.splice(i - 1, 3, parseInt(arr[i - 1]) * parseInt(arr[i + 1]));
+      //       i = 0;
+      //     } else if (arr[i] === "/" && arr[i + 1] && arr[i - 1]) {
+      //       arr.splice(i - 1, 3, parseInt(arr[i - 1]) / parseInt(arr[i + 1]));
+      //       i = 0;
+      //     } else if (arr[i] === "+" && arr[i + 1] && arr[i - 1]) {
+      //       arr.splice(i - 1, 3, parseInt(arr[i - 1]) + parseInt(arr[i + 1]));
+      //       i = 0;
+      //     } else if (arr[i] === "-" && arr[i + 1] && arr[i - 1]) {
+      //       arr.splice(i - 1, 3, parseInt(arr[i - 1]) - parseInt(arr[i + 1]));
+      //       i = 0;
+      //     }
+      //   }
+      //   return arr.join("");
     }
   };
 };
@@ -69,8 +49,10 @@ var calculate = new Calculator();
 console.log(calculate.evaluate("2 / 2 + 3 * 4 - 6"));
 
 //Psuedo Code
+//Two arrays with multindex and div index. Check both in a loop.
+//Check if the index of one is less than the other, if so operate on the lesser index.
 //{Refactor possibly with while loop?}
-//Check that the string contains both multiplication/division and addition/subtraction
+//Check that the arr contains both multiplication/division and addition/subtraction
 //If not operate left to write
 //Otherwise
 //(Figure out how to account for order of operations)
