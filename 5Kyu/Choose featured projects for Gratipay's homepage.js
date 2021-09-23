4e2ee88ff5e6f895ab293d6e03d28774745e77e8
Boston Rohan <bostonrohan@gmail.com>
2021-09-22 18:58:59 -0700

    worked on featured projects kata

--- 5Kyu/Choose featured projects for Gratipay's homepage.js
+++ 5Kyu/Choose featured projects for Gratipay's homepage.js
@@ -1,28 +1,47 @@
 function getFeaturedProjects(allProjects) {
+  const popular = getPopularUnpopular(allProjects)[0];
+  const unpopular = getPopularUnpopular(allProjects)[1];
   var featuredProjects = [];
   if (allProjects.length < 10) {
     return allProjects;
   } else {
-    const popularUnpopularSorted = getPopularUnpopular(allProjects);
+    let allProjectsCopy = allProjects.slice();
+    for (var i = 0; i < 10; i++) {
+      if (popular) {
+        featuredProjects.push(popular[Math.floor(Math.random() * i)]);
+        allProjectsCopy.splice(Math.floor(Math.random() * i), i);
+      } else {
+        featuredProjects.push(
+          allProjectsCopy[
+            Math.floor(Math.random() * (10 - featuredProjects.length))
+          ]
+        );
+      }
+    }
   }
 
   return featuredProjects;
 }
 //Helper function that sorts through the projects and places popular and unpopular projects in their respective arrays.
 function getPopularUnpopular(allProjects) {
-  if (allProjects.length < 10) {
-    return allProjects;
-  } else {
-    for (var i = 0; i < allProjects.length; i++) {
-      if (allProjects[i]["nreceiving_from"] > 5) {
-        popularProjects.push(allProjects[i]);
-      } else {
-        unpopularProjects.push(allProjects[i]);
-      }
+  var popularProjects,
+    unpopularProjects = [];
+  for (var i = 0; i < allProjects.length; i++) {
+    if (allProjects[i]["nreceiving_from"] > 5) {
+      popularProjects.push(allProjects[i]);
+    } else {
+      unpopularProjects.push(allProjects[i]);
     }
   }
-  return popularProjects, unpopularProjects;
+  if (popularProjects && unpopularProjects) {
+    return popularProjects, unpopularProjects;
+  } else if (popularProjects) {
+    return popularProjects;
+  } else {
+    return unpopularProjects;
+  }
 }
+console.log(getFakeProjects());
 //Conditions
 //Always return 10 projects or less
 //Seven of the projects should be drawn randomly from a pool of the popular projects (a project with 'nrecieving_from' > 5 is considered as popular)
