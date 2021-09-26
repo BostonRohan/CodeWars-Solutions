bd52af09f8677115472128771d2b77bc9d59d252
Boston Rohan <bostonrohan@gmail.com>
2021-09-25 18:10:03 -0700

    failed one test

--- 5Kyu/Choose featured projects for Gratipay's homepage.js
+++ 5Kyu/Choose featured projects for Gratipay's homepage.js
@@ -1,42 +1,46 @@
 function getFeaturedProjects(allProjects) {
   const popular = getPopularUnpopular(allProjects)[0];
   const unpopular = getPopularUnpopular(allProjects)[1];
+  var sortedPopular = Array.from(
+    Array(popular.length),
+    () => popular[Math.floor(Math.random() * popular.length)]
+  );
+  var sortedUnpopular = Array.from(
+    Array(unpopular.length),
+    () => unpopular[Math.floor(Math.random() * unpopular.length)]
+  );
   var featuredProjects = [];
   if (allProjects.length < 10) {
-    return allProjects;
-  }
-  //Syntax for filling array!!!
-  if (!popular.length) {
-    return Array.from(
-      Array(10),
+    featuredProjects = allProjects;
+  } else if (!popular.length) {
+    featuredProjects = sortedUnpopular;
+  } else if (!unpopular.length) {
+    featuredProjects = sortedPopular;
+  } else if (popular.length && popular.length < 7) {
+    sortedUnpopular = Array.from(
+      Array(10 - popular.length),
       () => unpopular[Math.floor(Math.random() * unpopular.length)]
     );
-  }
-  //--------------------
-  if (!unpopular.length) {
-    featuredProjects = new Array(10).fill(
-      popular[Math.random() * popular.length],
-      0,
-      10
+    featuredProjects = [...sortedPopular, ...sortedUnpopular];
+  } else if (unpopular.length && unpopular.length < 3) {
+    sortedPopular = Array.from(
+      Array(10 - unpopular.length),
+      () => popular[Math.floor(Math.random() * popular.length)]
     );
-  } else if (popular.length < 7) {
-    featuredProjects = new Array(10)
-      .fill(unpopular[Math.random() * unpopular.length], popular.length, 10)
-      .unshift(popular[Math.random() * popular.length]);
-  } else if (unpopular.length < 3) {
-    featuredProjects = new Array(10)
-      .fill(popular[Math.random() * popular.length], popular.length, 10)
-      .reverse()
-      .unshift(unpopular[Math.random() * unpopular.length]);
+    featuredProjects = [...sortedPopular, ...sortedUnpopular];
   } else {
+    let x = 0,
+      y = 0;
+    while (featuredProjects.length < popular.length) {
+      featuredProjects.unshift(sortedPopular[x]);
+      x++;
+    }
     while (featuredProjects.length < 10) {
-      featuredProjects.unshift(
-        popular[Math.floor(Math.random() * popular.length)].push(
-          unpopular[Math.floor(Math.random() * unpopular.length)]
-        )
-      );
+      featuredProjects.push(sortedUnpopular[y]);
+      y++;
     }
   }
+  return featuredProjects;
 }
 //Helper function that sorts through the projects and places popular and unpopular projects in their respective arrays.
 function getPopularUnpopular(allProjects) {
@@ -53,24 +57,23 @@ function getPopularUnpopular(allProjects) {
 }
 console.log(
   getFeaturedProjects([
+    { name: "Popular Project ", nreceiving_from: 9, receiving: 10 },
+    { name: "Popular Project ", nreceiving_from: 11, receiving: 10 },
+    { name: "Popular Project ", nreceiving_from: 9, receiving: 10 },
+    { name: "Popular Project ", nreceiving_from: 10, receiving: 10 },
+    { name: "Popular Project ", nreceiving_from: 7, receiving: 10 },
     { name: "Popular Project ", nreceiving_from: 2, receiving: 10 },
     { name: "Popular Project ", nreceiving_from: 1, receiving: 10 },
+    { name: "Popular Project ", nreceiving_from: 8, receiving: 10 },
+    { name: "Popular Project ", nreceiving_from: 12, receiving: 10 },
     { name: "Popular Project ", nreceiving_from: 4, receiving: 10 },
-    { name: "Popular Project ", nreceiving_from: 3, receiving: 10 },
-    { name: "Popular Project ", nreceiving_from: 1, receiving: 10 },
-    { name: "Popular Project ", nreceiving_from: 2, receiving: 10 },
-    { name: "Popular Project ", nreceiving_from: 1, receiving: 10 },
-    { name: "Popular Project ", nreceiving_from: 2, receiving: 10 },
-    { name: "Popular Project ", nreceiving_from: 3, receiving: 10 },
-    { name: "Popular Project ", nreceiving_from: 4, receiving: 10 },
-    // { name: "Popular Project ", nreceiving_from: 9, receiving: 10 },
-    // { name: "Popular Project ", nreceiving_from: 6, receiving: 10 },
-    // { name: "Popular Project ", nreceiving_from: 10, receiving: 10 },
   ])
 );
 //Conditions
-//Always return 10 projects or less
-//Seven of the projects should be drawn randomly from a pool of the popular projects (a project with 'nrecieving_from' > 5 is considered as popular)
+//sortedays return 10 projects or less
+//Seven of the projects should be dsortedn randomly from a pool of the popular projects (a projecsortedith 'nrecieving_from' > 5 is considered as popular)
 //The remaining three should be taken randomly from amongst the rest.
 //If there are less than 7 popular projects, or less than 3 unpopular projects, fill the gaps from the other group of projects if possible.
-//Randomize the order of results so that the popular objects don't always appear on top.
+//Randomize the order of results so that the popular objects don't sortedays appear on top.
+
+//https://stackoverfsorted.com/questions/1295584/most-efficiensorteday-to-create-a-zero-filled-javascript-array
