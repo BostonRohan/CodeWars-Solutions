7fd773aea1cf9eec6ddbc713e09942359fbf42fb
Boston Rohan <bostonrohan@gmail.com>
2021-10-05 17:20:47 -0700

    started research on next polydivisible number kata

---
+++ 4Kyu/Next polydivisible number in any base.js
@@ -0,0 +1,14 @@
+function next(b, n) {
+  if (!n[0] === 0) {
+  }
+}
+
+//Number will be between 2 and 62.
+//"A number is polydivisible if its first digit is cleanly divisible by 1, its first two digits by 2, its first three digits by 3 and so on."
+
+//According to https://en.wikipedia.org/wiki/Polydivisible_number , a polydivisble number has the following properties.
+//Its first digit a is not 0.
+//The number formed by its first two digits ab is a multiple of 2.
+//The number formed by its first three digits abc is a multiple of 3.
+//The number formed by its first four digits abcd is a multiple of 4.
+//etc.
