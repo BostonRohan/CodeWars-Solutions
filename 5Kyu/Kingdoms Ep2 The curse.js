67c7b377b5961df78259a2f1f00e9a98cbe24ffa
Boston Rohan <bostonrohan@gmail.com>
2021-10-20 16:07:04 -0700

    worked on kingdoms ep2 the curse kata

--- 5Kyu/Kingdoms Ep2 The curse.js
+++ 5Kyu/Kingdoms Ep2 The curse.js
@@ -1,16 +1,20 @@
-function translate(speech, vocabulary) {}
-function checkLength(speech, vocabulary) {
-  let speechWordLength = speech
-    .replace(/[?!,.]/g, "")
-    .split(" ")
-    .map((word) => word.length);
-  let vocabularyWordLength = vocabulary.map((word) => word.length);
+function translate(speech, vocabulary) {
+  let speechLetters = speech.replace(/[^A-Za-z0-9\s]/g, "").split("");
+  let phrase = [];
+  for (var i = 0; i < speechLetters.length; i++) {
+    for (var j = 0; j < vocabulary.length; j++) {
+      console.log(phrase, vocabulary[j], speechLetters[i]);
+      if (vocabulary[j].includes(speechLetters[i])) {
+        phrase.push(vocabulary[j]);
+      }
+    }
+  }
+  return phrase;
 }
-console.log(checkLength("cell, weak!", ["hell", "cell", "week", "weak"]));
-//Check length function
-//Split the string by words.
-//Check if there is punctuation on the speech string.
-//If remove it from the string, but save its value.
-//Check the length of each word in speech.
-//Iterate through the the words in the vocabulary
-//If the length is equal return true, it will be checked in main function
+
+console.log(translate("c**l, w*ak!", ["hell", "cell", "week", "weak"]));
+
+//Check if a word in vocaublary contains the available words in speech
+//If so push the word in vocabulary into the phrase
+//Start on the next word and repeat
+//Repeat until there are no words left in speech.
