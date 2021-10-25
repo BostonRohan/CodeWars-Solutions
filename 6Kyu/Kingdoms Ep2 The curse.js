6845e955a34d8d3f8874cdc108169d5abf9ddb94
Boston Rohan <bostonrohan@gmail.com>
2021-10-25 00:44:35 -0700

    committing correct version of kata

--- 6Kyu/Kingdoms Ep2 The curse.js
+++ 6Kyu/Kingdoms Ep2 The curse.js
@@ -1,36 +1,57 @@
 function translate(speech, vocabulary) {
+  //Removing punctuation and splitting into an array.
   let speechArr = speech.replace(/[^*A-Za-z0-9\s]/g, "").split(" ");
   let phrase = [];
   let punctuationData = savePunctuation(speech);
-  speechArr.forEach(word => {
-    vocabulary.forEach((vocabWord, index) => {
-      if(vocabWord.length === word.length){
+  speechArr.forEach((word) => {
+    vocabulary.forEach((vocabWord) => {
+      //If the vocab word and the speech word are the same length, start looking for matches.
+      if (vocabWord.length === word.length) {
         let match = 0;
-        for(var i = 0; i < word.length; i++){
-          if(word[i] === vocabWord[i] || word[i] === '*'){
+        for (var i = 0; i < word.length; i++) {
+          //If the letters are equal or speech letter is an asterik, then there is a match.
+          if (word[i] === vocabWord[i] || word[i] === "*") {
             match++;
+            //If match equals the length of the word then we have a matched word, push the vocab word into a new array.
             match === word.length ? phrase.push(vocabWord) : match;
-          }
-          else{
+          } else {
             match = 0;
           }
         }
       }
     });
   });
-  //Iterate through the object and place the appropriate punctuation at it's appropriate index.
-  return phrase.join(' ');
-}
-function savePunctuation(speech){
-  let punctuation = [];
-  let index = [];
-  for(var i = 0; i < speech.length; i++){
-    if((/[^*A-Za-z0-9\s]/g.test(speech[i]))){
-      punctuation.push(speech[i]);
-      index.push(i);
+  if (!phrase) {
+    return "";
+  } else {
+    phrase = phrase.join(" ").split("");
+    //Adding punctuation in at given index.
+    for (index in punctuationData) {
+      phrase.splice(index, 0, punctuationData[index]);
     }
   }
-  //Merge the two arrays into an object.
-  return punctuation;
+  return phrase.join("");
 }
-console.log(translate("***lo ..!w***d!", ["hello", "world"]));
+function savePunctuation(speech) {
+  let punctuation = [];
+  let punctuationIndex = [];
+  for (var i = 0; i < speech.length; i++) {
+    //If the character is not a letter or number, save it and it's index.
+    if (/[^*A-Za-z0-9\s]/g.test(speech[i])) {
+      punctuation.push(speech[i]);
+      punctuationIndex.push(i);
+    }
+  }
+  //Merge the two arrays into an object with the index functioning as the key, and punctuation as the value.
+  let punctuationObj = punctuation.reduce(function (
+    punctuationObj,
+    field,
+    index
+  ) {
+    punctuationObj[punctuationIndex[index]] = field;
+    return punctuationObj;
+  },
+  {});
+  return punctuationObj;
+}
+translate("c**l, w*ak!", ["hell", "cell", "week", "weak"]);
