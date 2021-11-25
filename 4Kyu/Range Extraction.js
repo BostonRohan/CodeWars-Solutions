87b8f54baa8bb7e5f6ad60afb2fb1d57488e1d0f
Boston Rohan <bostonrohan@gmail.com>
2021-11-25 13:17:25 -0800

    passed basic tests

--- 4Kyu/Range Extraction.js
+++ 4Kyu/Range Extraction.js
@@ -1,8 +1,8 @@
 function solution(list) {
-  let ranges = [];
   let match = 0;
   let startRange,
     endRange = 0;
+  let ans = [];
   for (let i = 0; i < list.length; i++) {
     let increment = list[i + 1] === list[i] + 1;
 
@@ -18,18 +18,16 @@ function solution(list) {
       }
       match = 0;
     }
+    if (match === 0 && i === 0) ans.push(list[i]);
+    if (!increment && endRange < startRange) ans.push(list[i - 1], list[i]);
     if (match === 0 && startRange < endRange) {
-      ranges.push(`${startRange}-${endRange}`);
+      ans.push(`${startRange}-${endRange}`);
     }
   }
-  return `ranges: ${ranges}, list: ${list}`;
+  return ans.join(",");
 }
 console.log(
   solution([
     -6, -3, -2, -1, 0, 1, 3, 4, 5, 7, 8, 9, 10, 11, 14, 15, 17, 18, 19, 20,
   ])
 );
-//Psuedo
-//Check a number
-//Iterate until the following number is no longer list[i] + 1
-//If the following number is no longer list[i] + 1, then splice the numbers and replace them with the first number in the iteration, a dash, and the last iteration.
