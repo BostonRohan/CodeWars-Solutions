a3d2b8f57cfae1ee7cccb305d867f9384bf8ad23
Boston Rohan <bostonrohan@gmail.com>
2021-12-06 20:24:33 -0800

    completed and annotated range extraction

--- 4Kyu/Range Extraction.js
+++ 4Kyu/Range Extraction.js
@@ -1,33 +1,31 @@
 function solution(list) {
-  let match = 0;
-  let startRange,
-    endRange = 0;
-  let ans = [];
-  for (let i = 0; i < list.length; i++) {
-    let increment = list[i + 1] === list[i] + 1;
-
-    if (increment) {
-      match++;
-    }
-    if (match === 1) {
-      startRange = list[i];
-    }
-    if (!increment) {
-      if (match >= 2) {
-        endRange = list[i];
+  //Establishing variables
+  let range = [];
+  let start = 0;
+  let end = 0;
+  //Iterate over list
+  for (let i = 0; i <= list.length; i++) {
+    //If the list increases properly then push it into the range arr
+    if (list[i] + 1 === list[i + 1]) {
+      range.push(list[i], list[i + 1]);
+    } else {
+      //Make a new unique array without duplicates
+      let uniqRange = [...new Set(range)];
+      //Reset range as the increment length is not sufficent
+      range = [];
+      if (uniqRange.length >= 3) {
+        //Establish our start and end range
+        start = uniqRange[0];
+        end = [uniqRange[uniqRange.length - 1]];
+        //Splice elements out of the list and replace them with the range
+        list.splice(list.indexOf(start), uniqRange.length, `${start}-${end}`);
+        //Going back to check other nodes since we're splicing
+        i = i - uniqRange.length;
       }
-      match = 0;
-    }
-    if (match === 0 && i === 0) ans.push(list[i]);
-    if (!increment && endRange < startRange) ans.push(list[i - 1], list[i]);
-    if (match === 0 && startRange < endRange) {
-      ans.push(`${startRange}-${endRange}`);
     }
   }
-  return ans.join(",");
+  return list.join(",");
 }
-console.log(
-  solution([
-    -6, -3, -2, -1, 0, 1, 3, 4, 5, 7, 8, 9, 10, 11, 14, 15, 17, 18, 19, 20,
-  ])
-);
+solution([
+  -6, -3, -2, -1, 0, 1, 3, 4, 5, 7, 8, 9, 10, 11, 14, 15, 17, 18, 19, 20,
+]);
